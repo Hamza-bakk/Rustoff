637e2fe24929f2171e0cd0d4dd8d79dd360b200e
Yunjidev <florian.vancamp@gmail.com>
2023-09-05 14:02:28 +0200

    Ajout navbar + icone panier et liens vers connexion et profil

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "home#index"
+  root 'static_pages#home'
 end
