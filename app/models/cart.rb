18c07ece71381645c75c7380dce54913c1366b33
Hamza <bakkar.hamza92@gmail.com>
2023-09-05 10:55:40 +0200

    finished models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  has_many :cart_items, dependent: :destroy
+  has_many :items, through: :cart_items
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_items
+  
 end
