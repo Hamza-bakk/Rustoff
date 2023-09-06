211df57f977cff3902a0d98b47d21d32606c7899
aglaeDev <a.gourdet.dev@gmail.com>
2023-09-06 09:58:57 +0200

    nul

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :carts
   resources :items
   devise_for :users
+
+  get 'shop/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
