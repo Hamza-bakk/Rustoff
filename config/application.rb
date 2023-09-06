8991e88a8265ba1bd37151bedce7fe51400611b0
Hamza <bakkar.hamza92@gmail.com>
2023-09-06 09:36:47 +0200

    finished boutiques

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -19,4 +20,5 @@ module Rustoff
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
+
 end
