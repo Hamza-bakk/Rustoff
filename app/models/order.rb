3fb3a5a21ddcad9445896adeb4c5418877db460e
Hamza <bakkar.hamza92@gmail.com>
2023-09-09 12:49:38 +0200

    Mise a jours des models pour supprimer les utilisateurs

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
 
   def total_price
     order_items.sum { |order_item| order_item.unit_price * order_item.quantity }
