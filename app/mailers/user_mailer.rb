b7d7b4b453ae5ad70a7236fd5efd4bedf6d59c2b
Hamza <bakkar.hamza92@gmail.com>
2023-09-13 14:22:09 +0200

    suppression end

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: 'rustoffgp@proton.me'
-
-    def welcome_email(user)
-      @user = user
-      @url  = 'www.rustoff-621db6cfed24.herokuapp.com'
   
-      mail(to: @user.email, subject: 'Bienvenue chez nous!', :host => 'www.rustoff-621db6cfed24.herokuapp.com')
-    end
+  def welcome_email(user)
+    @user = user
+    @url  = 'www.rustoff-621db6cfed24.herokuapp.com'
+    
+    mail(to: @user.email, subject: 'Bienvenue chez nous!', :host => 'www.rustoff-621db6cfed24.herokuapp.com')
   end
 end
