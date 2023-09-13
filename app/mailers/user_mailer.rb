bdfc87c82fccf70fc7b2a0a1bfc47a861a971453
Hamza <bakkar.hamza92@gmail.com>
2023-09-13 11:06:26 +0200

    Reset password views et mise a jour controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: 'rustoffgp@proton.me'
 
-  def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'www.rustoff-621db6cfed24.herokuapp.com' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue chez nous !') 
+    def welcome_email(user)
+      @user = user
+      @url  = 'www.rustoff-621db6cfed24.herokuapp.com'
+  
+      mail(to: @user.email, subject: 'Bienvenue chez nous!', :host => 'www.rustoff-621db6cfed24.herokuapp.com')
+    end
   end
-end
