f9ba002b57106cbea22387db60787e4d9d9bcc66
Hamza <bakkar.hamza92@gmail.com>
2023-09-13 10:50:10 +0200

    correction mailer avec proton

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "rustoffgp@hotmail.com"
+  default from: "rustoffgp@proton.me"
   layout "mailer"
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'rustoffgp@hotmail.com'
+  default from: 'rustoffgp@proton.me'
 
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
