96728871414b2545cf02712590a2e59ee1543070
Hamza <bakkar.hamza92@gmail.com>
2023-09-17 11:40:02 +0200

    Ajouter des statistiques coté User avec fonctionnalités a venir

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   # GET /users or /users.json
   def index
     @users = User.all
+    @num_orders = current_user.orders.count 
   end
 
   # GET /users/1 or /users/1.json
