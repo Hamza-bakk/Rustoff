0970d75df6be88631ffebf591f681b777695f95f
Hamza <bakkar.hamza92@gmail.com>
2023-09-18 11:23:10 +0200

    Mise a jour model Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items
+
 
   def total_price
     order_items.sum { |order_item| order_item.unit_price * order_item.quantity }
